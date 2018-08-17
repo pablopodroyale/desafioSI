c7d617817296dd02db3a746b3b1f025c4b8883a1
pablopodgaiz <pablopodgaiz@gmail.com>
2018-08-17 18:49:49 -0300

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -18,30 +18,7 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 
-         /*
-		 agregarAnimal(TipoAnimal.VACA, "lola", granja);
-		 agregarAnimal(TipoAnimal.VACA, "lola", granja);
-		
-		 granja.listarAnimales();
-         granja.listarAnimal("kjh");
-         granja.alimentarAnimal("lola");
-         granja.alimentarAnimales();
-         
-         granja.jugarConAnimal("lola");
-         granja.jugarConAnimales();
-         granja.descansarAnimal("lola");
-         granja.descansarAnimales();
-         */
 		input.close();
-	}
-
-	private static void agregarAnimal(TipoAnimal tipo, String nombre, Granja granja) {
-		try {
-			 granja.agregarAnimal(tipo, nombre);
-		} catch (RuntimeException re) {
-			System.out.println(re.getMessage());
-		}
 	}
 
 }
